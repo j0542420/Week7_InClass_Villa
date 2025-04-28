71428e18a609e1432bc66f25d380a8f8315ff121
Jose Villa <j0542420@amarillocollege.com>
2025-04-28 15:58:23 -0500

    Commit

---
+++ In-Class.js
@@ -0,0 +1,19 @@
+let click = "click";
+
+
+window.onload = function(){
+    if (type === " ")
+    {
+        document.getElementById("Full Name").addEventListener("click", window.onload);
+        document.getElementById("Age").addEventListener("click", window.onload);
+        ocument.getElementById("Email").addEventListener("click", window.onload);
+    }
+}
+
+const person = {
+    Querey: functon{
+        re
+    }
+}
+
+
