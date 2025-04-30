dee12df2f16294bd8a8e1f8c42ece41f2cd99bb6
Jose Villa <j0542420@amarillocollege.com>
2025-04-30 15:44:49 -0500

    update???

--- In-Class.js
+++ In-Class.js
@@ -1,19 +1,49 @@
-let click = "click";
+let add = document.getElementById("add");
 
+let display = document.getElementById("display");
+let customers = [];
 
-window.onload = function(){
-    if (type === " ")
+function Customer(name,age,email){
+    this.name = name;
+    this.age = age;
+    this.email = email;
+
+    this.display = function(){
+        return `  ---${this.name} --- ${this.age} --- ${this.email}`
+    }
+}
+
+add.onclick = function(){
+    //validation
+    let name = document.getElementById("Name");
+    let age = document.getElementById("Age");
+    let email = document.getElementById("Email");
+
+    let message = document.getElementById("message");
+    let totalQueue = document.getElementById("totalQueue");
+
+    if(name.value == ""|| age.value == ""|| email.value=="")
     {
-        document.getElementById("Full Name").addEventListener("click", window.onload);
-        document.getElementById("Age").addEventListener("click", window.onload);
-        ocument.getElementById("Email").addEventListener("click", window.onload);
+        //message: All fields must be filled
+        message.innerText = "all fields must be filled"
+    }
+    else
+    {
+        let customer = new Customer(name.value, age.value, email.value)
+        customers.push(customer);
+        totalQueue.innerText = "Total in queue: "+ customers.length;
     }
 }
 
-const person = {
-    Querey: functon{
-        re
+display.onclick = function(){
+    let list = document.getElementById("list");
+
+    let htmlCode = "No --- FullName ---- Age ---- Email<br>";
+    let order = 1;
+
+    for(let x in customers)
+    {
+        htmlCode+= (order++)+ x.display( + "<br>");
     }
+    list.innerHTML = htmlCode;
 }
-
-
